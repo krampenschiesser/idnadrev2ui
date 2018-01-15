4943e9f6f6562ff86cb04503bfa5abecc72f75b3
krampenschiesser <krampenschiesser@gmail.com>
2018-01-15 14:18:52 +0100

    cleanup javascript files

--- src/dto/FileType.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {value: true});
-var FileType;
-(function (FileType) {
-    FileType[FileType["Thought"] = 0] = "Thought";
-    FileType[FileType["Task"] = 1] = "Task";
-    FileType[FileType["Document"] = 2] = "Document";
-})(FileType = exports.FileType || (exports.FileType = {}));
