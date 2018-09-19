476da02375cf91a877fc3ab5c76d59bcf41bf675
krampenschiesser <krampenschiesser@gmail.com>
2018-09-19 12:58:09 -0400

    test for all value index

--- src/store/index/AllValueIndex.ts
+++ src/store/index/AllValueIndex.ts
@@ -1,72 +1,87 @@
-import Index from './Index';
+import {FileId} from '../../dto/FileId';
+import {FileType} from '../../dto/FileType';
 import IdnadrevFile from '../../dto/IdnadrevFile';
-import { FileType } from '../../dto/FileType';
-import { RepositoryId } from '../../dto/RepositoryId';
-import { FileId } from '../../dto/FileId';
+import {RepositoryId} from '../../dto/RepositoryId';
+import Index from './Index';
 
 export default class AllValueIndex<V> implements Index {
-  field: string;
-  values: Map<V | null, Set<FileId>> = new Map<V | null, Set<FileId>>();
-  inverse: Map<FileId, V | null> = new Map<FileId, V | null>();
-  type: FileType;
+    field: string;
+    values: Map<V | null, Set<FileId>> = new Map<V | null, Set<FileId>>();
+    inverse: Map<FileId, V | null> = new Map<FileId, V | null>();
+    type: FileType;
 
-  constructor(repo: RepositoryId, field: string, type: FileType) {
-    this.field = field;
-    this.type = type;
-  }
+    constructor(repo: RepositoryId, field: string, type: FileType) {
+        this.field = field;
+        this.type = type;
+    }
 
-  onUpdate(file: IdnadrevFile<any, any>): void {
-    if (file.fileType === this.type) {
-      let existing = this.inverse.get(file.id);
-      let fieldValue = file[this.field];
+    onUpdate(file: IdnadrevFile<any, any>): void {
+        if (file.fileType === this.type) {
+            let existing = this.inverse.get(file.id);
+            let fieldValue = file[this.field];
+            if (fieldValue === undefined) {
+                fieldValue = null;
+            }
 
-      fieldValue = typeof fieldValue === 'string' ? fieldValue.toLocaleLowerCase() : fieldValue;
-      if (existing && existing !== fieldValue) {
-        let set = this.values.get(existing);
-        if (set) {
-          set.delete(file.id);
+            fieldValue = typeof fieldValue === 'string' ? fieldValue.toLocaleLowerCase() : fieldValue;
+            if (existing !== undefined && existing !== fieldValue) {
+                let set = this.values.get(existing);
+                if (set) {
+                    set.delete(file.id);
+                    if(set.size===0) {
+                        this.values.delete(existing);
+                    }
+                }
+            }
+            let value = this.values.get(fieldValue);
+            if (!value) {
+                value = new Set<FileId>();
+                this.values.set(fieldValue, value);
+            }
+            value.add(file.id);
+            this.inverse.set(file.id, fieldValue);
         }
-      }
-      let value = this.values.get(null);
-      if (!value) {
-        value = new Set<FileId>();
-        this.values.set(null, value);
-      }
-      value.add(file.id);
-      this.inverse.set(file.id, fieldValue);
     }
-  }
 
-  onDelete(file: IdnadrevFile<any, any>): void {
-    if (file.fileType === this.type) {
-      let existing = this.inverse.get(file.id);
-      let fieldValue = file[this.field];
+    onDelete(file: IdnadrevFile<any, any>): void {
+        if (file.fileType === this.type) {
+            let existing = this.inverse.get(file.id);
+            let fieldValue = file[this.field];
+            if(fieldValue===undefined) {
+                fieldValue=null;
+            }
 
-      fieldValue = typeof fieldValue === 'string' ? fieldValue.toLocaleLowerCase() : fieldValue;
-      if (existing && existing !== fieldValue) {
-        let set = this.values.get(existing);
-        if (set) {
-          set.delete(file.id);
+            fieldValue = typeof fieldValue === 'string' ? fieldValue.toLocaleLowerCase() : fieldValue;
+            if (existing !==undefined && existing !== fieldValue) {
+                let set = this.values.get(existing);
+                if (set) {
+                    set.delete(file.id);
+                    if(set.size===0){
+                        this.values.delete(existing);
+                    }
+                }
+            }
+            let value = this.values.get(fieldValue);
+            if (value) {
+                value.delete(file.id);
+                if(value.size===0) {
+                    this.values.delete(fieldValue);
+                }
+            }
+            this.inverse.delete(file.id);
         }
-      }
-      let value = this.values.get(null);
-      if (value) {
-        value.delete(file.id);
-      }
-      this.inverse.delete(file.id);
     }
-  }
 
-  getAllValues(): Set<V | null> {
-    return new Set<V | null>(Array.from(this.values.keys()));
-  }
-
-  getIds(key: V | null): Set<FileId> {
-    let set = this.values.get(key);
-    if (!set) {
-      return new Set();
-    } else {
-      return set;
+    getAllValues(): Set<V | null> {
+        return new Set<V | null>(Array.from(this.values.keys()));
     }
-  }
+
+    getIds(key: V | null): Set<FileId> {
+        let set = this.values.get(key);
+        if (!set) {
+            return new Set();
+        } else {
+            return set;
+        }
+    }
 }
---
+++ src/store/index/test/AllValuedIndexTest.test.ts
@@ -0,0 +1,57 @@
+import {FileType} from "../../../dto/FileType";
+import Task from "../../../dto/Task";
+import AllValueIndex from "../AllValueIndex";
+
+
+test('CRUD', () => {
+    let index = new AllValueIndex('test', 'context', FileType.Task);
+    let task = new Task("test");
+    task.details.context = "hello";
+    index.onUpdate(task);
+
+    let value = index.getAllValues().values().next().value;
+    expect(value).toEqual('hello');
+
+    task.details.context = "world";
+    index.onUpdate(task);
+
+    let allValues = index.getAllValues();
+    expect(allValues.size).toEqual(1);
+    value = allValues.values().next().value;
+    expect(value).toEqual('world');
+
+    index.onDelete(task);
+
+    allValues = index.getAllValues();
+    expect(allValues.size).toEqual(0);
+
+    index.onUpdate(task);
+    task.details.context = 'other';
+    index.onDelete(task);
+    allValues = index.getAllValues();
+    expect(allValues.size).toEqual(0);
+    expect(index.inverse.size).toEqual(0);
+});
+
+test('NullValue', () => {
+    let index = new AllValueIndex('test', 'context', FileType.Task);
+    let task = new Task("test");
+    index.onUpdate(task);
+
+    let value = index.getAllValues().values().next().value;
+    expect(value).toEqual(null);
+
+    index.onDelete(task);
+    let allValues = index.getAllValues();
+    expect(allValues.size).toEqual(0);
+    expect(index.inverse.size).toEqual(0);
+
+    task.details.context = "world";
+    index.onUpdate(task);
+    task.details.context = undefined;
+    index.onDelete(task);
+
+    allValues = index.getAllValues();
+    expect(allValues.size).toEqual(0);
+    expect(index.inverse.size).toEqual(0);
+});
