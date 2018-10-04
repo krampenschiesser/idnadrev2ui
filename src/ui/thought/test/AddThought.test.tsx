4302e7f44e48a724c595ade61c4e00ca5d94ed8b
krampenschiesser <krampenschiesser@gmail.com>
2018-10-04 08:57:34 -0400

    cleanup

--- src/ui/thought/test/AddThought.test.tsx
+++ src/ui/thought/test/AddThought.test.tsx
@@ -15,7 +15,6 @@ configure({ adapter: new Adapter() });
 
 
 test('Store repository and open with indexes', async () => {
-  <NameFormItem file={} form={}/>
   jest.setTimeout(30000);
   let repo = new Repository('test', 'test');
   await store.webStorage.storeRepository(repo);
@@ -25,6 +24,7 @@ test('Store repository and open with indexes', async () => {
   expect(store.openRepositories.length).toEqual(1);
 
   let rendered = shallow(<AddThought store={store} uiStore={new UiStore()} children={[]}/>);
+  console.log(rendered)
   rendered.find(NameFormItem).find('input').value = "bla";
-  rendered.find('.ant-btn').simulate('onClick')
+  rendered.find('button').simulate('click')
 });
